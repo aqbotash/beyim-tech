69c68974e77b60021848160cfef038d2b4b67e02
Kamila <tashimovakamila1@gmail.com>
2024-04-13 14:56:52 +0500

    Custom

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import IELTSTest
+from .models import IELTSTest, CustomUser
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['username','email', 'password']
+        model = CustomUser
+        fields = ['username','email', 'password', 'category']
         extra_kwargs = {
             'password': {'write_only': True},  # Ensure password is write-only
         }
