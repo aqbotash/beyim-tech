0490471a49dfbcd76528f89cfcc144c0f4d0c6c1
Kamila <tashimovakamila1@gmail.com>
2024-04-13 14:00:39 +0500

    CustomUser

--- api/models.py
+++ api/models.py
@@ -1,17 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class CostomUser(AbstractUser):
+class CustomUser(AbstractUser):
     ROLE_CHOICES = (
         ('student', 'student'),
         ('parent', 'parent'),
-        ('techer', 'techer')
+        ('teacher', 'teacher')
     )
     id = models.AutoField(primary_key=True)
     category = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
     
     def __str__(self):
         return self.username
+
+class Teacher(CustomUser):
+    lessons = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Parent(CustomUser):
+    payments = models.FloatField()
+    def __str__(self):
+        return self.name
+
+class Student(CustomUser):
+    parent = models.ForeignKey(Parent, on_delete=models.CASCADE)
+    teachers = models.ManyToManyField(Teacher)
+
 
 class IELTSTest(models.Model):
     id = models.AutoField(primary_key=True)
@@ -20,6 +36,6 @@ class IELTSTest(models.Model):
     listening_score = models.FloatField()
     speaking_score = models.FloatField()
     writing_score = models.FloatField()
-    participant_id = models.ForeignKey('Student', on_delete=models.CASCADE)
+    participant_id = models.ForeignKey(Student, on_delete=models.CASCADE)
     
     
