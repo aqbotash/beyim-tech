53449c3959970ee6e01310cd6c804c9d261ee0dc
Kamila <tashimovakamila1@gmail.com>
2024-04-13 13:33:50 +0500

    feat: CostomUser model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CostomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('student', 'student'),
+        ('parent', 'parent'),
+        ('techer', 'techer')
+    )
+    id = models.AutoField(primary_key=True)
+    category = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
+    
+    def __str__(self):
+        return self.username
+
