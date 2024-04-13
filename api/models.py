bef92fa33c3e1a093d5eb32e1e2387e47a85e616
Akbota <aqbotamaratqyzy@gmail.com>
2024-04-13 13:25:40 +0500

    feat: Test model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+
+
+class IELTSTest(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.DateField()
+    reading_score = models.FloatField()
+    listening_score = models.FloatField()
+    speaking_score = models.FloatField()
+    writing_score = models.FloatField()
+    participant_id = models.ForeignKey('Student', on_delete=models.CASCADE)
+    
+    
