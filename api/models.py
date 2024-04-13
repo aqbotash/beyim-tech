ace07deb35021d3675b5954fd8e6a2f4ae87430f
Akbota <aqbotamaratqyzy@gmail.com>
2024-04-13 14:10:04 +0500

    feat: serializers

--- api/models.py
+++ api/models.py
@@ -1,34 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-class CustomUser(AbstractUser):
-    ROLE_CHOICES = (
-        ('student', 'student'),
-        ('parent', 'parent'),
-        ('teacher', 'teacher')
-    )
-    id = models.AutoField(primary_key=True)
-    category = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
-    
-    def __str__(self):
-        return self.username
-
-class Teacher(CustomUser):
-    lessons = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-class Parent(CustomUser):
-    payments = models.FloatField()
-    def __str__(self):
-        return self.name
-
-class Student(CustomUser):
-    parent = models.ForeignKey(Parent, on_delete=models.CASCADE)
-    teachers = models.ManyToManyField(Teacher)
-
-
+# Create your models here.
 class IELTSTest(models.Model):
     id = models.AutoField(primary_key=True)
     date = models.DateField()
@@ -38,4 +10,5 @@ class IELTSTest(models.Model):
     writing_score = models.FloatField()
     participant_id = models.ForeignKey(Student, on_delete=models.CASCADE)
     
+
     
