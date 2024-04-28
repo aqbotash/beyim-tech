e5deeddb2c74c8a8e4341470982227163feec75b
Akbota <aqbotamaratqyzy@gmail.com>
2024-04-28 15:45:49 +0500

    feat: dashboard

---
+++ event-management-system-front/src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+// DashboardPage.tsx
+import EventList from '@/components/dashboard/EventList';
+import React from 'react';
+
+const DashboardPage: React.FC = () => {
+  return (
+    <div>
+        <EventList/>
+    </div>
+  );
+};
+
+export default DashboardPage;
